6933f0ff4e32ed18f05c7b319f3ff7e300b66980
pequenojoohn <kheymk@hotmail.com>
2023-03-01 10:30:58 -0300

    feat: create action click navbar

--- src/components/navbar/index.tsx
+++ src/components/navbar/index.tsx
@@ -1,13 +1,32 @@
+import { useState } from "react";
 import { BsList } from "react-icons/bs";
+import { MdOutlineClose } from "react-icons/md";
 
 import { Container } from "./styles";
 
 const Navbar = () => {
+
+    const [open, setOpen] = useState(false);
+
+    function handleOpenMenu() {
+        const navbar = document.querySelector("nav");
+        setOpen(open => !open);
+
+        //Caso open for true
+        if (open) {
+            return navbar?.classList.toggle("open");
+        }
+        navbar?.classList.toggle("open");
+    }
+
     return (
         <Container>
             <img src="./assets/logo-white.png" alt="" />
-            <BsList size={44}/>
-
+            {open ?
+                <MdOutlineClose size={44} onClick={() => handleOpenMenu()} />
+                :
+                <BsList size={44} onClick={() => handleOpenMenu()} />
+            }
             <nav>
                 <ul>
                     <li>A experiência</li>
--- src/components/navbar/styles.ts
+++ src/components/navbar/styles.ts
@@ -4,15 +4,61 @@ export const Container = styled.header`
     display: flex;
     align-items: center;
     justify-content: space-around;
-    height: 128px;
     background-color: #2E7BA2;
-
+    height: 128px;
+    position: relative;
+    
     img {
         width: 233px;
     }
     
     svg {        
         color: #FFFFFF;
+    }
+
+    nav {
+        content: "";
+        position: absolute;
+        width: 100%;
+
+        left: 100%;
+        top: 100%;
+        background-color: #2E7BA2;
+
+        transition: 0.5s all;
+        
+        ul {
+            width: 100%;
+
+            li {
+                padding: 1rem;
+                position: relative;
+                text-align: center;
+                color: #FFFFFF;
+                font-weight: 700;
+
+                &:last-child {
+                    &:after {
+                        display: none;
+                    }
+                }
+
+                &:after {
+                    content: "";
+                    width: 90%;
+                    bottom: 0;
+                    left: 0;
+                    transform: translate(5%, 0%);
+                    position: absolute;
+                    background-color: #FFFFFF;
+                    height: 1px;
+                }
+            }
+        }
+    }
+
+    .open {
+        left: 0%;
     }
 
     @media screen and (min-width: 768px) {
